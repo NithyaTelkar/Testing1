c0a0986324981ab88e339af803acfc672146dafd
Nithya T P <87232897+NithyaTelkar@users.noreply.github.com>
2023-02-03 16:07:15 +0530

    Add files via upload

---
+++ Holidaylist/src/main/java/Testing2/Holidaylist/App.java
@@ -0,0 +1,13 @@
+package Testing2.Holidaylist;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
